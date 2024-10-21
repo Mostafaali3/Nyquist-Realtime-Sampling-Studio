a6218dfd7ed5157618b17a43bb047d1c0da35b13
Mostafaali3 <mostafaalisarhan@gmail.com>
2024-10-21 20:52:18 +0300

    Update signalComponent.py

--- classes/signalComponent.py
+++ classes/signalComponent.py
@@ -1,6 +1,8 @@
+import numpy as np
 class SignalComponent():
-    def __init__(self):
-        self.__amplitude = 0
-        self.__frequency = 0
-        self.__shift = 0
+    def __init__(self, amplitude, frequency, shift):
+        self.__amplitude = amplitude
+        self.__frequency = frequency
+        self.__shift = shift
+        self.__id = np.random.randint(100000, size=1)[0]
         
