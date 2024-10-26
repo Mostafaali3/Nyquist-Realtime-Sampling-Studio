cc40376d212310d90a2bef967a1d98bb3a6bb9fb
kareem <kareem.abdelnabi.22@gmail.com>
2024-10-26 19:49:53 +0300

    changes

--- main.py
+++ main.py
@@ -29,27 +29,26 @@ class MainWindow(QMainWindow):
 
         # self.components_layout = self.componentsContainerWidget.layout()
 
-        # add_component(self.grid_layout, 1)
-        # add_component( self.grid_layout, 2)
-        # add_component(self.grid_layout, 3)
-        # add_component(self.grid_layout, 4)
-        # add_component(self.grid_layout, 5)
-        # add_component(self.grid_layout, 6)
-        # add_component(self.grid_layout, 7)
-        # add_component(self.grid_layout, 8)
-        # add_component(self.grid_layout, 9)
-        # add_component(self.grid_layout, 10)
+        add_component(self.components_grid_layout, 1)
+        add_component( self.components_grid_layout, 2)
+        add_component(self.components_grid_layout, 3)
+        add_component(self.components_grid_layout, 4)
+        add_component(self.components_grid_layout, 5)
+        add_component(self.components_grid_layout, 6)
+        add_component(self.components_grid_layout, 7)
+        add_component(self.components_grid_layout, 8)
+        add_component(self.components_grid_layout, 9)
+        add_component(self.components_grid_layout, 10)
 
-        # self.signals_layout = self.signalsContainerWidget.layout()
+        self.signals_layout = self.signalsContainerWidget.layout()
 
-        # add_signal(self.signals_layout, 1)
-        # add_signal(self.signals_layout, 2)
-        # add_signal(self.signals_layout, 3)
-        # add_signal(self.signals_layout, 4)
-        # add_signal(self.signals_layout, 5)
-        # add_signal(self.signals_layout, 6)
-        # add_signal(self.signals_layout, 7)
-
+        add_signal(self.signals_layout, 1)
+        add_signal(self.signals_layout, 2)
+        add_signal(self.signals_layout, 3)
+        add_signal(self.signals_layout, 4)
+        add_signal(self.signals_layout, 5)
+        add_signal(self.signals_layout, 6)
+        add_signal(self.signals_layout, 7)
 
 
         #######
