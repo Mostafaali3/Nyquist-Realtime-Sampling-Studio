a47dcbbf206c279230b59d5145f21eba9341324f
kareem <kareem.abdelnabi.22@gmail.com>
2024-10-29 21:58:03 +0300

    fix

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-        self.signals_layout = self.signalsContainerWidget.layout()
-
-        add_signal(self.signals_layout, 1)
-        add_signal(self.signals_layout, 2)
-        add_signal(self.signals_layout, 3)
-        add_signal(self.signals_layout, 4)
-        add_signal(self.signals_layout, 5)
-        add_signal(self.signals_layout, 6)
-        add_signal(self.signals_layout, 7)
+current_components
